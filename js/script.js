1d7616981853c14514dc9d98f5c4c23312233277
Gustavo Feriani <guuh.feriani@gmail.com>
2024-05-11 11:30:47 -0300

    update js tema

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//
+
+
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+const accordionHeader = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function changeTheme() {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+// função para alternar Tema "dark" / "light"
+
+
+
+
+
